ba7490d95c45bb7d5609d08306443b17b197ae34
bahstuff <92510275+bahstuff@users.noreply.github.com>
2021-12-16 18:14:54 +0100

    Update app.js

--- HOTEL/app.js
+++ HOTEL/app.js
@@ -1,45 +1,52 @@
 //NUMERO DE NOCHES HOTEL
-let nightNumber;
+var nightNumber;
 var getnightNumber = () => {
     nightNumber = parseInt(document.getElementById("nights").value);
+    console.log("nºnoches: " + nightNumber);
     return nightNumber;
 }
 document.getElementById("nights").addEventListener("keyup",getnightNumber)
 
 
 // //TIPO DE HABITACION
-let roomType;
+// var roomType = () => parseInt(document.getElementById("roomtype").value);
+// document.getElementById("roomtype").addEventListener("mouseout",roomType)
+var roomType;
 var getroomType = () => {
     roomType = parseInt(document.getElementById("roomtype").value);
+    console.log("Precio por tipo de habitación: " + roomType + "€");
     return roomType;
 }
 document.getElementById("roomtype").addEventListener("change",getroomType)
 
 //OCUPACION DE HABITACION
-let roomOcc;
+var roomOcc;
 var roomOccupance = () => {
     roomOcc = document.getElementById("roomocupance").value;
+    console.log("Tarifa extra/descuento: " + roomOcc + "€");
     return roomOcc;
 }
 document.getElementById("roomocupance").addEventListener("change",roomOccupance)
 
 //OPCION SPA
-let spa = 0;
+var spa = 0;
 function spaService() {
     var spaService = document.getElementById('spaServ');
     if(spaService.checked){
         spa = 20;
-        alert("Has seleccionado el servicio de SPA")
+        alert("Has seleccionado el servicio SPA")
     } else {
         spa = 0;
     }
+    console.log("Precio Spa :" + spa + " €");
     return spa
 }
 
 //NUMERO NOCHES PARKING
-let parkingNights;
+var parkingNights;
 var getparkingPrice = () => {
     parkingNights = parseInt(document.getElementById("parking").value)* 10;
+    console.log("Precio total Parking: " + parkingNights + " €");
     return parkingNights;
 }
 document.getElementById("parking").addEventListener("keyup",getparkingPrice)
@@ -47,11 +54,12 @@ document.getElementById("parking").addEventListener("keyup",getparkingPrice)
 
 //CALCULO TOTAL
 function calculate() {
+    console.log("--------CALCULO------");
     let totalBase = roomType * nightNumber + spa;
+    console.log("Total base: " + totalBase + " €");
     let totalIncremento = eval(totalBase + totalBase * roomOcc);
+    console.log("Total incremento/descuento: " + totalIncremento + " €");
     let totalReserva = totalIncremento + parkingNights;
+    console.log("Precio de la reserva: " + totalReserva + " €");
     document.getElementById("totalPrice").value = totalReserva + " €";
-}
-    
-
-
+    }
